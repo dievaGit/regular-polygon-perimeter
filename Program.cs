2d64e185bf012fa2455ca4059c9ab4741b381cbd
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:11:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetroPoligonoRegular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float lado = 0.0f;
+            int cantLado = 0;
+            float perimetro = 0.0f;
+
+            Console.Write("Escriba cuanto mide un lado del poligono:");
+            lado = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Escriba la cantidad de lados del poligono:");
+            cantLado = Convert.ToInt32(Console.ReadLine());
+
+            perimetro = lado * cantLado;
+
+            Console.WriteLine("El perimetro del poligono regular es: {0} unidades", perimetro);
+
+            Console.ReadKey();
+
+        }
+    }
+}
